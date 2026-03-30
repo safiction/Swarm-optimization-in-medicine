0ca82e968ef13d3af7b10a60b4da0889dbb9e084
katyazavr <salazkina2007@gmail.com>
2026-03-30 09:38:46 +0300

    Update baseline_models.py

--- notebooks/baseline_models.py
+++ notebooks/baseline_models.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pandas as pd
+from src.pso import PSOFeatureSelector
 
 sys.path.append(os.path.abspath("../"))
 
@@ -30,13 +31,51 @@ for name, model in models.items():
     model.fit(X_train, y_train)
     results[name] = evaluate(model, X_test, y_test)
 
-# Printing results
-for name, res in results.items():
-    print(f"\n{name}")
-    print("Accuracy:", res["accuracy"])
-    print("F1 (macro):", res["f1_macro"])
-
 df_results = pd.DataFrame(results).T
 df_results = df_results[["accuracy", "f1_macro"]]
 
 df_results.sort_values(by="f1_macro", ascending=False)
+
+print("\nRunning PSO...")
+
+pso = PSOFeatureSelector(
+    n_particles=20,
+    n_iterations=20
+)
+
+pso.fit(X_train, y_train)
+
+selected_features = X_train.columns[pso.best_features_]
+
+print("\nSelected features:")
+print(selected_features.tolist())
+
+selected_features.to_series().to_csv(
+    "../results/selected_features.csv",
+    index=False
+)
+
+X_train_reduced = X_train[selected_features]
+X_test_reduced = X_test[selected_features]
+
+
+print("\n=== AFTER PSO ===")
+
+results_pso = {}
+
+for name, model in models.items():
+    print(f"\nTraining (PSO): {name}")
+    
+    model.fit(X_train_reduced, y_train)
+    results_pso[name] = evaluate(model, X_test_reduced, y_test)
+
+df_before = pd.DataFrame(results).T
+df_after = pd.DataFrame(results_pso).T
+
+comparison = df_before[["accuracy", "f1_macro"]].copy()
+comparison.columns = ["acc_before", "f1_before"]
+
+comparison["acc_after"] = df_after["accuracy"]
+comparison["f1_after"] = df_after["f1_macro"]
+
+print(comparison)
